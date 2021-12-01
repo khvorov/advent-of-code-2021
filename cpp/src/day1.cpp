5a6504ee7bf448e3eab853fd39711054648ceac8
Dmitry Khvorov <dmitry.khvorov@gmail.com>
2021-12-01 13:23:18 +0800

    day1 solution

--- cpp/src/day1.cpp
+++ cpp/src/day1.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <limits>
+#include <deque>
+#include <numeric>
 
 int main()
 {
+    int prev = std::numeric_limits<int>::max();
+    int prev_smoothed = std::numeric_limits<int>::max();
+    int count = 0, count_smoothed = 0;
+
+    std::deque<int> window;
+
+    for (int d; std::cin >> d; )
+    {
+        if (d > prev) {
+            ++count;
+        }
+
+        prev = d;
+
+        // smoothed calc
+        window.push_back(d);
+        if (window.size() > 3) {
+            window.pop_front();
+        }
+
+        if (window.size() == 3) {
+            auto sum = std::accumulate(window.begin(), window.end(), 0);
+
+            if (sum > prev_smoothed) {
+                ++count_smoothed;
+            }
+
+            prev_smoothed = sum;
+        }
+    }
+
+    std::cout << count << ", " << count_smoothed << '\n';
+
     return 0;
 }
